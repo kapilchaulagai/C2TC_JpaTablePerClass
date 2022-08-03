fa8c736d065c86e26dc92c02f8e191e363825ccb
cw_51 <cw_51@ASUS-BEAST>
2022-08-03 21:42:44 +0530

    Commit on 03-08-2022

---
+++ JpaTablePerClass/src/com/cg/jpatableperclass/entities/InheritanceTest.java
@@ -0,0 +1,34 @@
+package com.cg.jpatableperclass.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one employee
+		Employee employee = new Employee();
+		employee.setName("John");
+		employee.setSalary(5000);
+		em.persist(employee);
+		
+		//create one manager
+		Manager manager = new Manager();
+		manager.setName("Trisha");
+		manager.setSalary(8000);
+		em.persist(manager);
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added one employee and manager to database.");
+		em.close();
+		factory.close();
+	}
+}
+
